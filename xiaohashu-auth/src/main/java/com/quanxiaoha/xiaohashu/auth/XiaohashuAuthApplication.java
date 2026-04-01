be5f538be6c185a6371e1b09834b1eee8481bff7
朱天宇 <1019427682@qq.com>
2026-04-01 23:57:37 +0800

    Spring Boot 3.x 整合 MyBatis

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplication.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/XiaohashuAuthApplication.java
@@ -1,9 +1,11 @@
 package com.quanxiaoha.xiaohashu.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.quanxiaoha.xiaohashu.auth.domain.mapper")
 public class XiaohashuAuthApplication {
 
     public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class XiaohashuAuthApplication {
     }
 
 }
+
