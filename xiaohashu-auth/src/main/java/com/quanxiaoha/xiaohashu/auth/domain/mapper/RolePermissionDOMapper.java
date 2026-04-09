1602a9d3cfd4e8a259d61698bb2b06afb87a27d1
朱天宇 <1019427682@qq.com>
2026-04-09 21:44:18 +0800

    同步【角色-权限集合】数据到 Redis 中

--- xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/domain/mapper/RolePermissionDOMapper.java
+++ xiaohashu-auth/src/main/java/com/quanxiaoha/xiaohashu/auth/domain/mapper/RolePermissionDOMapper.java
@@ -1,6 +1,9 @@
 package com.quanxiaoha.xiaohashu.auth.domain.mapper;
 
 import com.quanxiaoha.xiaohashu.auth.domain.dataobject.RolePermissionDO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface RolePermissionDOMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +17,12 @@ public interface RolePermissionDOMapper {
     int updateByPrimaryKeySelective(RolePermissionDO record);
 
     int updateByPrimaryKey(RolePermissionDO record);
+
+    /**
+     * 根据角色 ID 集合批量查询
+     *
+     * @param roleIds
+     * @return
+     */
+    List<RolePermissionDO> selectByRoleIds(@Param("roleIds") List<Long> roleIds);
 }
