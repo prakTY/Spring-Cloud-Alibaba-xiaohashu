38da9bca9c89f9c30fcfa0cc810e25e5ef66d0fd
朱天宇 <1019427682@qq.com>
2026-04-15 22:11:35 +0800

    优化：网关登录校验失败提示信息（友好化）

--- xiaohashu-gateway/src/main/java/com/quanxiaoha/xiaohashu/gateway/auth/SaTokenConfigure.java
+++ xiaohashu-gateway/src/main/java/com/quanxiaoha/xiaohashu/gateway/auth/SaTokenConfigure.java
@@ -1,6 +1,10 @@
 package com.quanxiaoha.xiaohashu.gateway.auth;
 
 import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
+import cn.dev33.satoken.exception.NotRoleException;
+import cn.dev33.satoken.exception.SaTokenException;
 import cn.dev33.satoken.reactor.filter.SaReactorFilter;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
@@ -40,8 +44,21 @@ public class SaTokenConfigure {
 
                     // 更多匹配 ...  */
                 })
-
+                // 异常处理方法：每次setAuth函数出现异常时进入
+                .setError(e -> {
+                    // return SaResult.error(e.getMessage());
+                    // 手动抛出异常，抛给全局异常处理器
+                    if (e instanceof NotLoginException) { // 未登录异常
+                        throw new NotLoginException(e.getMessage(), null, null);
+                    } else if (e instanceof NotPermissionException || e instanceof NotRoleException) { // 权限不足，或不具备角色，统一抛出权限不足异常
+                        throw new NotPermissionException(e.getMessage());
+                    } else { // 其他异常，则抛出一个运行时异常
+                        throw new RuntimeException(e.getMessage());
+                    }
+                })
                 ;
+
+
     }
 }
 
